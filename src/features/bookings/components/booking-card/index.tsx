fed351425f760dcfcf21e2da6166051d9c23f9ba
Neven Dyulgerov <neven.diulgerov@gmail.com>
2026-02-27 13:07:11 +0200

    feat: Add functionality for deleting a booking

--- src/features/bookings/components/booking-card/index.tsx
+++ src/features/bookings/components/booking-card/index.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { type FC, useCallback, useState } from "react";
 import { Button, Flex, Icon, Image, Link, Stack, Text } from "@chakra-ui/react";
 import type { PropertyModel } from "@/store/slices/properties/types";
 import { Card } from "@/common/components/ui/card";
@@ -11,6 +11,7 @@ import { PropertyRating } from "@/features/properties/components/property-rating
 import { Link as RouterLink } from "react-router";
 import type { BookingModel } from "@/store/slices/bookings/types";
 import { useProperty } from "@/features/properties/hooks/use-property";
+import { DeleteBookingDialog } from "@/features/bookings/components/delete-booking-dialog";
 
 interface BookingCardProps {
   booking: BookingModel;
@@ -21,70 +22,99 @@ export const BookingCard: FC<BookingCardProps> = (props) => {
   const { booking, imageSize = "small" } = props;
   const property = useProperty(booking.propertyId) as PropertyModel;
   const pathToProperty = `/bookings/${booking.id}`;
+  const [isDeleteBookingDialogActive, setDeleteBookingDialogActive] =
+    useState(false);
+
+  const onDeleteBooking = useCallback(() => {
+    setDeleteBookingDialogActive(true);
+  }, []);
 
   return (
-    <Card height="100%">
-      <Flex gap={4}>
+    <>
+      <DeleteBookingDialog
+        booking={booking}
+        isActive={isDeleteBookingDialogActive}
+        onChangeActive={setDeleteBookingDialogActive}
+      />
+
+      <Card height="100%">
         <Flex gap={4}>
-          <RouterLink to={pathToProperty}>
-            <Image
-              src={property.imageUrl}
-              loading="lazy"
-              boxSize={
-                imageSize === "small"
-                  ? "240px"
-                  : imageSize === "medium"
-                    ? "480px"
-                    : "100%"
-              }
-              objectFit="cover"
-              borderRadius="xl"
-            />
-          </RouterLink>
-        </Flex>
-        <Stack gap={4} flex={1}>
-          <Flex justifyContent="space-between" gap={1} flexWrap="wrap">
-            <Stack gap={2}>
-              <Flex flexWrap="wrap" gap={2}>
-                <Link asChild>
-                  <RouterLink to={pathToProperty}>
-                    <Text fontSize="xl" fontWeight="700">
-                      {property.name}
-                    </Text>
-                  </RouterLink>
-                </Link>
+          <Flex gap={4}>
+            <RouterLink to={pathToProperty}>
+              <Image
+                src={property.imageUrl}
+                loading="lazy"
+                boxSize={
+                  imageSize === "small"
+                    ? "240px"
+                    : imageSize === "medium"
+                      ? "480px"
+                      : "100%"
+                }
+                objectFit="cover"
+                borderRadius="xl"
+              />
+            </RouterLink>
+          </Flex>
+          <Stack gap={4} flex={1}>
+            <Flex justifyContent="space-between" gap={1} flexWrap="wrap">
+              <Stack gap={2}>
+                <Flex flexWrap="wrap" gap={2}>
+                  <Link asChild>
+                    <RouterLink to={pathToProperty}>
+                      <Text fontSize="xl" fontWeight="700">
+                        {property.name}
+                      </Text>
+                    </RouterLink>
+                  </Link>
 
-                <PropertyRating rating={property.rating} />
-              </Flex>
+                  <PropertyRating rating={property.rating} />
+                </Flex>
 
-              <Flex alignItems="center" gap={1}>
-                <Icon as={LuMapPin} />
-                <Text fontSize="xs" fontWeight="700">
-                  {property.location}
-                </Text>
-              </Flex>
+                <Flex alignItems="center" gap={1}>
+                  <Icon as={LuMapPin} />
+                  <Text fontSize="xs" fontWeight="700">
+                    {property.location}
+                  </Text>
+                </Flex>
+              </Stack>
+
+              <PropertyReviewScore
+                reviewScore={property.reviewScore}
+                reviewsCount={property.reviewsCount}
+              />
+            </Flex>
+
+            <Stack gap={2} flex={1}>
+              <PropertyType type={property.type} />
+              <PropertyFacilities facilities={property.facilities} />
             </Stack>
 
-            <PropertyReviewScore
-              reviewScore={property.reviewScore}
-              reviewsCount={property.reviewsCount}
-            />
-          </Flex>
+            <Stack gap={2} flex={1} alignItems="flex-end">
+              <PropertyPrice price={property.price} />
 
-          <Stack gap={2} flex={1}>
-            <PropertyType type={property.type} />
-            <PropertyFacilities facilities={property.facilities} />
+              <Flex alignItems="center" gap={2}>
+                <Button
+                  colorPalette="orange"
+                  variant="solid"
+                  borderColor="errorForeground"
+                  backgroundColor="errorBackground/10"
+                  _hover={{
+                    backgroundColor: "errorBackground/40",
+                  }}
+                  onClick={onDeleteBooking}
+                >
+                  Delete Booking
+                </Button>
+
+                <Button colorPalette="orange" variant="solid" asChild>
+                  <RouterLink to={pathToProperty}>Edit Booking</RouterLink>
+                </Button>
+              </Flex>
+            </Stack>
           </Stack>
-
-          <Stack gap={2} flex={1} alignItems="flex-end">
-            <PropertyPrice price={property.price} />
-
-            <Button colorPalette="orange" variant="solid" asChild>
-              <RouterLink to={pathToProperty}>Edit Booking</RouterLink>
-            </Button>
-          </Stack>
-        </Stack>
-      </Flex>
-    </Card>
+        </Flex>
+      </Card>
+    </>
   );
 };
---
+++ src/features/bookings/components/delete-booking-dialog/index.tsx
@@ -0,0 +1,82 @@
+import { type FC, useCallback, useState } from "react";
+import { Button, Dialog, Portal } from "@chakra-ui/react";
+import { useNavigate } from "react-router";
+import { toaster } from "@/common/components/ui/toaster/toaster";
+import { useAppDispatch } from "@/store/store";
+import type { BookingModel } from "@/store/slices/bookings/types";
+import { deleteBooking } from "@/store/slices/bookings";
+import { useUserBookings } from "@/features/bookings/hooks/use-user-bookings";
+
+interface DeleteBookingDialogProps {
+  booking: BookingModel;
+  isActive: boolean;
+  onChangeActive: (isActive: boolean) => void;
+}
+
+export const DeleteBookingDialog: FC<DeleteBookingDialogProps> = (props) => {
+  const { booking, isActive, onChangeActive } = props;
+  const userBookings = useUserBookings();
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const dispatch = useAppDispatch();
+
+  const onConfirm = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      dispatch(deleteBooking({ booking }));
+      onChangeActive(false);
+
+      const pathname = userBookings.length > 1 ? "/bookings" : "/properties";
+      navigate(pathname);
+      setLoading(false);
+    } catch {
+      toaster.create({
+        title:
+          "Error while deleting the booking. Please try again or contact support.",
+        type: "error",
+      });
+      setLoading(false);
+    }
+  }, [booking, dispatch, navigate, onChangeActive, userBookings.length]);
+
+  return (
+    <Dialog.Root
+      placement="center"
+      closeOnEscape={false}
+      closeOnInteractOutside={false}
+      lazyMount
+      open={isActive}
+      onOpenChange={(e) => onChangeActive(e.open)}
+    >
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>
+                Are you sure you want to delete this booking?
+              </Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button variant="outline" disabled={isLoading}>
+                  Cancel
+                </Button>
+              </Dialog.ActionTrigger>
+              <Button
+                variant="solid"
+                colorPalette="orange"
+                loading={isLoading}
+                loadingText="Deleting booking..."
+                onClick={onConfirm}
+              >
+                Delete booking
+              </Button>
+            </Dialog.Footer>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+};
