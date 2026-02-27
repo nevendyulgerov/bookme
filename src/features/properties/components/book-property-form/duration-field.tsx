a728b3854ed9690cda0d6d0df49e01703f4db066
Neven Dyulgerov <neven.diulgerov@gmail.com>
2026-02-27 13:03:44 +0200

    feat: Disable already booked dates / Add duration details

--- src/features/properties/components/book-property-form/duration-field.tsx
+++ src/features/properties/components/book-property-form/duration-field.tsx
@@ -1,39 +1,80 @@
-import type { FC } from "react";
+import { type FC } from "react";
 import { Controller, useFormContext } from "react-hook-form";
-import { Field } from "@chakra-ui/react";
+import { Field, Flex, Stack, Text } from "@chakra-ui/react";
 import { DurationPicker } from "@/common/components/ui/duration-picker";
 import { isObject } from "lodash";
+import { format, isEqual } from "date-fns";
+import {
+  getAlreadyBookedDaysForPropertyBookings,
+  getDifferenceInDays,
+} from "@/features/properties/components/book-property-form/utils";
+import type { PropertyModel } from "@/store/slices/properties/types";
+import { usePropertyBookings } from "@/features/bookings/hooks/use-property-bookings";
 
 interface DurationFieldProps {
+  property: PropertyModel;
   isExistingBooking: boolean;
 }
 
-export const DurationField: FC<DurationFieldProps> = ({
-  isExistingBooking,
-}) => {
+export const DurationField: FC<DurationFieldProps> = (props) => {
+  const { property, isExistingBooking } = props;
+  const propertyBookings = usePropertyBookings(property);
   const { control } = useFormContext();
+  const alreadyBookedDates =
+    getAlreadyBookedDaysForPropertyBookings(propertyBookings);
 
   return (
     <Controller
       control={control}
       name="duration"
       render={({ field: { onChange, value }, fieldState: { error } }) => {
+        const isEndDateDifferentFromStartDate =
+          isObject(value?.to) && !isEqual(value.from, value.to);
+
         return (
-          <Field.Root required invalid={!!error} width="auto">
-            <DurationPicker
-              range={value}
-              defaultMonth={isExistingBooking ? value.from : new Date()}
-              // disabledDates={[new Date(2026, 1, 11), new Date(2026, 1, 12)]}
-              error={error?.message}
-              onChangeRange={(val) => {
-                const nextVal = isObject(val)
-                  ? val
-                  : { from: undefined, to: undefined };
-                onChange(nextVal);
-              }}
-            />
-            <Field.ErrorText>{error?.message as string}</Field.ErrorText>
-          </Field.Root>
+          <Stack>
+            <Field.Root required invalid={!!error} width="auto">
+              <DurationPicker
+                range={value}
+                defaultMonth={isExistingBooking ? value.from : new Date()}
+                disabledDates={alreadyBookedDates}
+                error={error?.message}
+                onChangeRange={(val) => {
+                  const nextVal = isObject(val)
+                    ? val
+                    : { from: undefined, to: undefined };
+                  onChange(nextVal);
+                }}
+              />
+              <Field.ErrorText>{error?.message as string}</Field.ErrorText>
+            </Field.Root>
+
+            {value?.from && (
+              <Stack alignItems="flex-end">
+                <Flex justifyContent="space-between" gap={4}>
+                  <Stack gap={2}>
+                    <Text>From:</Text>
+                    {isEndDateDifferentFromStartDate && <Text>To:</Text>}
+                    <Text>Day(s):</Text>
+                  </Stack>
+
+                  <Stack gap={2} justifyContent="flex-end">
+                    <Text fontWeight="700" textAlign="right">
+                      {format(value.from, "dd/MM/yyyy")}
+                    </Text>
+                    {isEndDateDifferentFromStartDate && (
+                      <Text fontWeight="700" textAlign="right">
+                        {format(value.to, "dd/MM/yyyy")}
+                      </Text>
+                    )}
+                    <Text fontWeight="700" textAlign="right">
+                      {getDifferenceInDays(value)}
+                    </Text>
+                  </Stack>
+                </Flex>
+              </Stack>
+            )}
+          </Stack>
         );
       }}
     />
